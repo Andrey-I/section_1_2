a375b2e36204c9d1f35b42bb1b976ca6d1279477
Andrey Garnagin <agarnagin@exadel.com>
2017-11-30 23:02:51 +0200

    initial commit

---
+++ src/main/java/org/andrey/spring/section_1_2/DemoApplication.java
@@ -0,0 +1,12 @@
+package org.andrey.spring.section_1_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
---
+++ src/test/java/org/andrey/spring/section_1_2/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.andrey.spring.section_1_2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
